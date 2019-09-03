2b9754af954a79d415a18b66673e18f68a3388db
Masster <andrei80586@gmail.com>
2019-09-03 20:15:20 +0300

    New type of requests

--- app/src/main/java/com/masstersoft/weathernordic/MainActivity.kt
+++ app/src/main/java/com/masstersoft/weathernordic/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.masstersoft.weathernordic
 
-import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -28,22 +27,22 @@ class MainActivity : AppCompatActivity() {
                     call: Call<CurrentWeather>,
                     response: Response<CurrentWeather>
                 ) {
-                    Log.d(Constants.TAG, response.body().toString())
+                    parseResponse(response)
                 }
             })
 
 
-        object : AsyncTask<Any, Any, Response<CurrentWeather>>() {
-            override fun doInBackground(vararg p0: Any?): Response<CurrentWeather> {
-                return App.api
-                    .getCurrentWeather(34.0, 34.9)
-                    .execute()
-            }
-
-            override fun onPostExecute(result: Response<CurrentWeather>) {
-                Log.d(Constants.TAG, result.toString())
-            }
-        }.execute()
+//        object : AsyncTask<Any, Any, Response<CurrentWeather>>() {
+//            override fun doInBackground(vararg p0: Any?): Response<CurrentWeather> {
+//                return App.api
+//                    .getCurrentWeather(34.0, 34.9)
+//                    .execute()
+//            }
+//
+//            override fun onPostExecute(result: Response<CurrentWeather>) {
+//                Log.d(Constants.TAG, result.errorBody().toString())
+//            }
+//        }.execute()
 
 //        Log.d(Constants.TAG,
 //            App.api
@@ -52,5 +51,42 @@ class MainActivity : AppCompatActivity() {
 //                .body()
 //                .toString())
     }
+
+    fun parseResponse(result: Response<CurrentWeather>) {
+
+//        when (result.code()) {
+//            in 200..299 -> Log.d(Constants.TAG, result.toString())
+//            else -> Log.d(Constants.TAG, result.errorBody().toString())
+//        }
+//
+//        if(result.isSuccessful){
+//            Log.d(Constants.TAG, result.toString())
+//        }else{
+//            Log.d(Constants.TAG, result.errorBody().toString())
+//        }
+
+//        Log.d(
+//            Constants.TAG, when (result.code()) {
+//                in 200..299 -> result.toString()
+//                else -> result.errorBody().toString()
+//            }
+//        )
+
+        Log.d(Constants.TAG, getResponse(result))
+
+    }
+
+//    fun getResponse(result: Response<CurrentWeather>): String = if (result.isSuccessful) {
+//        result.toString()
+//    } else {
+//        result.errorBody().toString()
+//    }
+
+    fun getResponse(result: Response<CurrentWeather>): String =
+        when (result.code()) {
+            in 200..299 -> result.toString()
+            else -> result.errorBody().toString()
+        }
+
 
 }
